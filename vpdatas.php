402577a33284f49cae08a1d9544a64f1d95f9be9
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2024-04-13 11:23:37 +0100

    latest now

--- vpdatas.php
+++ vpdatas.php
@@ -71,7 +71,7 @@ require __DIR__.'/plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/bikendi-tech-solutions/vpdatas',
+	'https://github.com/bikendi-tech-solutions/vpdatas/',
 	__FILE__,
 	'vpdatas'
 );
